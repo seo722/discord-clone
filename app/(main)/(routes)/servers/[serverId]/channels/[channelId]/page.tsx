0c2dd891bc85be20e769d62db432b57690b4152b
seo722 <isogood11@naver.com>
2023-10-05 11:01:11 +0900

    socket io setup

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,8 +2,9 @@ import { redirectToSignIn } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 import { currentProfile } from '@/lib/current-profile';
-import { db } from '@/lib/db';
 import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '@/components/chat/chat-input';
+import { db } from '@/lib/db';
 
 interface ChannelIdPageProps {
   params: {
@@ -39,6 +40,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
   );
 };
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+interface ChatInputProps {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: 'conversation' | 'channel';
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
+export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: '',
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  };
+
+  return (
+    <div>
+      <div>chat input</div>
+    </div>
+  );
+};
